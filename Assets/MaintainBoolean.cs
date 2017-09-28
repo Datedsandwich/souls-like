ef971db715c5cdea6e802d3abd83ebb6e63803a6
Luke Robinson <luke.robinson@blackcatsolutions.co.uk>
2017-09-28 13:18:45 +0100

    Animator for the box man, set up with locomotion and attacks. Simple script to manipulate the animators parameters

---
+++ Assets/MaintainBoolean.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainBoolean : StateMachineBehaviour {
+	public string booleanName;
+	public bool booleanState;
+
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		animator.SetBool(booleanName, booleanState);
+	}
+
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		animator.SetBool(booleanName, !booleanState);
+	}
+}
---
+++ Assets/Scripts/HumanoidAnimationController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanoidAnimationController : MonoBehaviour {
+
+	[Range(0, 1)]
+	public float vertical;
+
+	[Range(0, 1)]
+	public float horizontal;
+
+	[Range(0, 1)]
+	public int weaponType;
+
+	public bool isApplyingRootMotion;
+	public bool isAttacking;
+
+	public string[] oneHandedAttacks;
+	public string[] twoHandedAttacks;
+
+	private Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		isApplyingRootMotion = animator.GetBool("ApplyRootMotion");
+		animator.applyRootMotion = isApplyingRootMotion;
+
+		if(isApplyingRootMotion) {
+			return;
+		}
+
+		animator.SetInteger("WeaponType", weaponType);
+
+		if(isAttacking) {
+			string targetAnimation;
+
+			if(weaponType == 0) {
+				int attack = Random.Range(0, oneHandedAttacks.Length);
+				targetAnimation = oneHandedAttacks[attack];
+			} else {
+				int attack = Random.Range(0, twoHandedAttacks.Length);
+				targetAnimation = twoHandedAttacks[attack];
+			}
+
+			vertical = 0;
+			animator.CrossFade(targetAnimation, 0.2f);
+			animator.SetBool("ApplyRootMotion", true);
+			isAttacking = false;
+		} else {
+			animator.SetFloat("Vertical", vertical);
+			animator.SetFloat("Horizontal", horizontal);
+		}
+	}
+}
